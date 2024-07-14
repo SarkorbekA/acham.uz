a9be772bb25f1503bb71d2ab5a70b29293220866
Sarkor <abilkosimovsarkor@gmail.com>
2024-07-14 21:11:43 +0500

    first commit

---
+++ backend/admin.py
@@ -0,0 +1,50 @@
+from django.contrib import admin
+from .models import *
+
+
+
+# Register your models here.
+
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    ordering = ['id']
+    list_display_links = ['id', 'name']
+
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    ordering = ['id']
+    list_display_links = ['id', 'name']
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'material', 'collection', 'price']
+    ordering = ['id']
+    list_display_links = ['id', 'name']
+
+
+
+class AboutUsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    ordering = ['id']
+    list_display_links = ['id', 'title']
+
+
+class CountBlogAboutAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    ordering = ['id']
+    list_display_links = ['id', 'title']
+
+
+
+
+
+
+
+
+admin.site.register(Collection, CollectionAdmin)
+admin.site.register(Material, MaterialAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(AboutUs, AboutUsAdmin)
+admin.site.register(CountBlogAbout, CountBlogAboutAdmin)
+
---
+++ backend/urls.py
@@ -0,0 +1,46 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('second_page/', second_page, name='second_page'),
+    path('third_page/<int:pk>', third_page, name='third_page'),
+    path('fourth_page/', fourth_page, name='fourth_page'),
+    path('search/', search_collections, name='search_collections'),
+
+
+
+
+    # en
+    path('index_en', index_en, name='index_en'),
+    path('second_page_en/', second_page_en, name='second_page_en'),
+    path('third_page_en/<int:pk>', third_page_en, name='third_page_en'),
+    path('fourth_page_en/', fourth_page_en, name='fourth_page_en'),
+    path('search_en/', search_collections_en, name='search_collections_en'),
+
+
+    # uz
+    path('index_uz', index_uz, name='index_uz'),
+    path('second_page_uz/', second_page_uz, name='second_page_uz'),
+    path('third_page_uz/<int:pk>', third_page_uz, name='third_page_uz'),
+    path('fourth_page_uz/', fourth_page_uz, name='fourth_page_uz'),
+    path('search_uz/', search_collections_uz, name='search_collections_uz'),
+
+
+    # actions with cart
+    path('add/<item_id>\)', cart_add, name='cart_add'),
+    path('remove/<item_id>', cart_remove, name='cart_remove'),
+    path('save_cart/', save_cart, name='save_cart'),
+
+    # actions with cart en
+    path('add_en/<item_id>\)', cart_add_en, name='cart_add_en'),
+    path('remove_en/<item_id>', cart_remove_en, name='cart_remove_en'),
+    path('save_cart_en/', save_cart_en, name='save_cart_en'),
+
+
+# actions with cart uz
+    path('add_uz/<item_id>\)', cart_add_uz, name='cart_add_uz'),
+    path('remove_uz/<item_id>', cart_remove_uz, name='cart_remove_uz'),
+    path('save_cart_uz/', save_cart_uz, name='save_cart_uz')
+
+]
